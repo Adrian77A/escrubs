26f5ab0cee72451abefedd16feeffb9c97759ea4
Adrian77A <adrianalfredo717@gmail.com>
2025-04-30 21:04:06 -0600

    add service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\Policies\BannerPolicy;
 use Illuminate\Support\ServiceProvider;
 use Amendozaaguiar\FilamentRouteStatistics\FilamentRouteStatisticsPlugin;
 use Filament\Panel;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,7 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-      //
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
     
     public function panel(Panel $panel): Panel
